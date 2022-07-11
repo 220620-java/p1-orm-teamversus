bc313bbb4accec5aa8be91934663b335b33484eb
evgeniyko99 <ekk4357@gmail.com>
2022-07-11 17:47:25 -0400

    Making the ORM

--- src/main/java/VersusAppDriver.java
+++ src/main/java/VersusAppDriver.java
@@ -1,6 +1,14 @@
+import com.revature.versusapp.data.UserORM;
+import com.revature.versusapp.models.User;
 
 public class VersusAppDriver {
+	private static UserORM userORM = new UserORM();
 	public static void main (String[] args) {
+		User user = new User(1, "Yujiin", "1234", "Evgeniy", "Ko");
+		userORM.create(user);
+	}
+	
+	private static void setupVersusLib() {
 		
 	}
 }
--- src/main/java/com/revature/versusapp/data/UserORM.java
+++ src/main/java/com/revature/versusapp/data/UserORM.java
@@ -1,15 +1,58 @@
 package com.revature.versusapp.data;
 
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.revature.versusapp.models.User;
+import com.revature.versusapp.utils.ConnectionUtil;
 
 public class UserORM implements UserDAO{
-
+	private ConnectionUtil connUtil = ConnectionUtil.getConnectionUtil();
 	@Override
-	public User create(User t) {
-		// TODO Auto-generated method stub
-		return null;
+	public User create(User newUser) {
+		
+		try (Connection conn = connUtil.getConnection()){
+			conn.setAutoCommit(false);
+			StringBuilder sql = new StringBuilder();
+			Class objectClass = newUser.getClass();
+			sql.append("insert into person values (");
+			for(Field field : objectClass.getDeclaredFields()) {
+				field.setAccessible(true);
+				if (field.getName().equals("id")) {
+					sql.append("default, ");
+				} else if (field.getType().isPrimitive()) {
+					sql.append(field.get(newUser) + ", ");
+				} else {
+					sql.append("'" + field.get(newUser) + "', ");
+				}
+			}
+			sql.delete(sql.length()-2, sql.length());
+			sql.append(");");
+			System.out.println(sql);
+			PreparedStatement stmt = conn.prepareStatement(sql.toString());
+			int rowsAffected = stmt.executeUpdate();
+			ResultSet resultSet = stmt.getGeneratedKeys();
+			if (resultSet.next() && rowsAffected == 1) {
+				newUser.setId(resultSet.getInt("id"));
+				conn.commit();
+			} else {
+				conn.rollback();
+				return null;
+			}
+			
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return newUser;
 	}
 
 	@Override
