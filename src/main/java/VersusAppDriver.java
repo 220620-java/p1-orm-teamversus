3763287519d6f0b8de454f9c4ed8fc445a26e5de
evgeniyko99 <ekk4357@gmail.com>
2022-07-14 14:42:42 -0400

    fixing ORM

--- src/main/java/VersusAppDriver.java
+++ src/main/java/VersusAppDriver.java
@@ -4,13 +4,11 @@ import com.revature.versusapp.models.Person;
 public class VersusAppDriver {
 	private static ORM ORM = new ORM();
 	public static void main (String[] args) {
-		Person user = new Person("Yujiin", "1234", "Evgeniy", "Ko");
-		//ORM.create(user);
-		Person test = new Person(4);
-		test = (Person) ORM.findById(test);
+//		Person user = new Person("Evgeniy", "1234", "Evgeniy", "Ko");
+//		ORM.create(user);
+		Person test = new Person(25);
+//		ORM.delete(test);
+		System.out.println(ORM.findById(test));
 	}
 	
-	private static void setupVersusLib() {
-		
-	}
 }
